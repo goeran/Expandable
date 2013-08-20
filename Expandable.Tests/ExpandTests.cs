7f216b1fa9413f39b86db4bd499a35eadcadf18a
goeran <mail@goeran.no>
2013-08-20 20:44:48 +0200

    init commit of code

---
+++ Expandable.Tests/ExpandTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Expandable.Tests
+{
+    class ExpandTests
+    {
+        [TestFixture]
+        public class When_expanding_table
+        {
+            [Test]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void It_requires_string_to_be_specified()
+            {
+                Expand.Table(null);
+            }
+
+            [Test]
+            [ExpectedException(typeof(ArgumentException), ExpectedMessage = "String can't be empty")]
+            public void It_requires_string_to_contain_data()
+            {
+                Expand.Table("");
+            }
+
+            [Test]
+            public void It_will_return_an_Expand_object()
+            {
+                Assert.IsInstanceOf<Expand>(Expand.Table("some inputData"));
+            }
+        }
+
+        [TestFixture]
+        public class When_expanding_table_to_list_of_objects
+        {
+            private IEnumerable<Person> employees;
+            private Person steve;
+            private Person bill;
+
+            [SetUp]
+            public void Setup()
+            {
+                employees = Expand.Table(@"
+                    Name    | Age | Salary      | HasPhone
+                    Steve   | 55  | 150000      | true
+                    Bill    | 56  | 160000.10   | false
+                ").ToListOf<Person>();
+                steve = employees.ElementAt(0);
+                bill = employees.ElementAt(1);
+}
+
+            [Test]
+            public void It_will_not_return_a_null_pointer()
+            {
+                Assert.IsNotNull(employees);
+            }
+
+            [Test]
+            public void It_will_return_back_the_same_number_of_objects_as_rows_in_table()
+            {
+                Assert.AreEqual(2, employees.Count());
+            }
+
+            [Test]
+            public void It_is_able_to_set_properties_of_type_string()
+            {
+                Assert.AreEqual("Steve", steve.Name);
+                Assert.AreEqual("Bill", bill.Name);
+            }
+
+            [Test]
+            public void It_is_able_to_set_properties_of_type_int()
+            {
+                Assert.AreEqual(55, steve.Age);
+                Assert.AreEqual(56, bill.Age);
+            }
+
+            [Test]
+            public void It_is_able_to_set_properties_of_type_double()
+            {
+                Assert.AreEqual(150000, steve.Salary);
+                Assert.AreEqual(160000.10, bill.Salary);
+            }
+
+            [Test]
+            public void It_is_able_to_set_properties_of_type_boolean()
+            {
+                Assert.IsTrue(steve.HasPhone);
+                Assert.IsFalse(bill.HasPhone);
+            }
+
+            [Test]
+            public void It_will_properties_with_complex_types()
+            {
+                Assert.IsNull(steve.Address);
+            }
+        }
+    }
+
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public bool HasPhone { get; set; }
+        public Address Address { get; set; }
+    }
+
+    internal class Address
+    {
+        public String StreetName { get; set; }
+        public int StreetNumber { get; set; }
+    }
+}
---
+++ Expandable/Expand.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+public class Expand
+{
+    private string table;
+    private static readonly NumberFormatInfo americanNumberFormat;
+
+    private Expand()
+    {
+    }
+
+    static Expand()
+    {
+        americanNumberFormat = new CultureInfo("en-US").NumberFormat;
+    }
+
+    public static Expand Table(String inputData)
+    {
+        if (inputData == null) throw new ArgumentNullException();
+        if (String.IsNullOrEmpty(inputData)) throw new ArgumentException("String can't be empty");
+
+        var expand = new Expand();
+        expand.table = inputData;
+
+        return expand;
+    }
+
+    public IEnumerable<T> ToListOf<T>()
+    {
+        var result = new List<T>();
+        String header = null;
+        var columns = new List<String>();
+        var rows = new List<String>();
+
+        using (var reader = new StringReader(table))
+        {
+            while (reader.Peek() != -1)
+            {
+                var line = reader.ReadLine();
+                if (!String.IsNullOrWhiteSpace(line))
+                {
+                    if (header == null)
+                    {
+                        header = line;
+                        columns.AddRange(header.Split('|').Select(c => c.Trim()));
+                    }
+                    else
+                    {
+                        rows.Add(line);
+                    }
+                }
+            }
+        }
+
+        foreach (var row in rows)
+        {
+            var rowColumns = row.Split('|');
+            var obj = Activator.CreateInstance<T>();
+            var properties = obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.SetProperty | BindingFlags.Public);
+            foreach (var property in properties)
+            {
+                for (int colIndex = 0; colIndex < columns.Count; colIndex++)
+                {
+                    if (property.Name == columns[colIndex])
+                    {
+                        Object val = rowColumns[colIndex];
+                        val = TypeConversion(property, val);
+                        property.SetValue(obj, val, new object[0]);
+                        break;
+                    }
+                }
+            }
+            result.Add(obj);
+        }
+
+        return result;
+    }
+
+    private static object TypeConversion(PropertyInfo property, object val)
+    {
+        if (property.PropertyType == typeof (String))
+            return Convert.ToString(val).Trim();
+        if (property.PropertyType == typeof (Int32))
+            return Convert.ToInt32(val);
+        if (property.PropertyType == typeof (Double))
+            return Convert.ToDouble(val, americanNumberFormat);
+        if (property.PropertyType == typeof (Boolean))
+            return Convert.ToBoolean(val);
+        return null;
+    }
+}
